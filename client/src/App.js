73bb07448abacdd3a7132702732101f280d07d21
YoungwanCho <developer119korea@gmail.com>
2021-01-17 21:01:45 +0900

    서버에서 고객정보를 받아 보여주도록 수정

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,23 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '철수',
-    'birthday': '891222',
-    'gender': '남자',
-    'job': '디자이너'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '영희',
-    'birthday': '891222',
-    'gender': '여자',
-    'job': '프로그래머'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '이순신',
-    'birthday': '891222',
-    'gender': '남자',
-    'job': '백수'
-  }
-]
-
 class App extends Component {
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -66,7 +55,7 @@ class App extends Component {
             </TableHead>
             <TableBody>
               {
-                customers.map(c => {
+                this.state.customers ? this.state.customers.map(c => {
                   return (
                     <Customer
                       key={c.id}
@@ -78,7 +67,7 @@ class App extends Component {
                       job={c.job}
                     />
                   )
-                })
+                }) : ""
               }
             </TableBody>
           </Table>
